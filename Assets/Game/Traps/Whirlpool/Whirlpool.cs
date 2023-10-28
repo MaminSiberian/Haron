571943ffe70e1921aa748b6b3f31613aa00a53cd
Co6op13 <Co6op13@yandex.ru>
2023-10-28 15:47:56 +0500

    pfkbdf.

---
+++ Assets/Game/Traps/Whirlpool/Whirlpool.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Whirlpool : MonoBehaviour
+{
+    [SerializeField] private float forceAttraction;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Vector3 direction;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            Debug.LogWarning(collision.gameObject + " run into whirpool");
+            rb = collision.GetComponent<Rigidbody2D>();            
+        }
+
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (rb != null)
+        {
+            Debug.Log("attraction");
+            direction = (transform.position - collision.transform.position).normalized;
+            rb.velocity = direction * forceAttraction * Time.deltaTime;
+            //rb.AddForce(forceAttraction * direction * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        rb = null;
+    }
+}
