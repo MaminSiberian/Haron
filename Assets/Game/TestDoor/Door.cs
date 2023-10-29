df3456b328569553b5941af48e0441476dd6b57a
Co6op13 <Co6op13@yandex.ru>
2023-10-29 22:25:30 +0500

    door

--- Assets/Game/TestDoor/Door.cs
+++ Assets/Game/TestDoor/Door.cs
@@ -14,6 +14,7 @@ public class Door : MonoBehaviour
         isOpen = false;
         _anim = GetComponent<Animator>();
         canOpen = false;
+        GameplayUI = FindObjectOfType<GameplayUI>();
     }
 
     
