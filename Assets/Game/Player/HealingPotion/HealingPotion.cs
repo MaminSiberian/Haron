51f3fdf26ab3dfcea9faabcf278437e23c567ee9
Co6op13 <Co6op13@yandex.ru>
2023-10-29 10:01:54 +0500

    Хилка работае

---
+++ Assets/Game/Player/HealingPotion/HealingPotion.cs
@@ -0,0 +1,16 @@
+using Haron;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealingPotion : MonoBehaviour
+{
+    [SerializeField] private int givesHP;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<IHealing>() != null)
+        {
+            collision.GetComponent<IHealing>().GetHeal(givesHP);
+        }
+    }
+}
