7cf8ee019c18c439c49f25241777fc2881329ac8
ZhekaRifer <scorpionrulit2@bk.ru>
2023-10-29 15:26:31 +0300

    Add audio

--- Assets/Game/Souls/Soul.cs
+++ Assets/Game/Souls/Soul.cs
@@ -8,10 +8,18 @@ public class Soul : MonoBehaviour
     private int _marinaId;
     private Color _color;
     public SpriteRenderer _renderer;
+    [Range(9, 14)]public float minCooldown;
+    [Range(15, 30)]public float maxCooldown;
+    private float cooldown;
+    public AudioClip[] FX;
+    private AudioSource _source;
+    private float currentCooldown;
 
     private void Start()
     {
-        
+        _source = GetComponent<AudioSource>();
+        currentCooldown = cooldown;
+        cooldown = Random.Range(minCooldown, maxCooldown);
         if(_soulData != null )
         {
             _index = _soulData.Index;
@@ -29,7 +37,15 @@ public class Soul : MonoBehaviour
 
     private void Update()
     {
-        
+        if(currentCooldown < 0 && !_source.isPlaying)
+        {
+            _source.PlayOneShot(FX[Random.Range(0, FX.Length - 1)]);
+            currentCooldown = cooldown;
+        }
+        else if(currentCooldown > 0)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
     }
 
     public int GetIndex() { return _index; }
